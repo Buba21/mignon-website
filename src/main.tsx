9821deed25fa57d9d3f87a15623244f17ba579f2
Diogo Moreira <dmoreira.buba@gmail.com>
2026-04-01 16:59:35 +0100

    fix icons import

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n'
 import App from './App.tsx'
+import 'flag-icons/css/flag-icons.min.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
